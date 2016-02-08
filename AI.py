3e61ca6f9d8c9c9d8907501a6e514ed3f54c9c87
pezzati <p.ezzati.ab@gmail.com>
2016-02-08 19:25:00 +0330

    Models debugged

--- AI.py
+++ AI.py
@@ -2,6 +2,9 @@ __author__ = 'AmirHS'
 
 
 class AI():
-
     def do_turn(self, world):
-        world.move_army(3, 2, 4)
+        my_nodes = world.get_my_nodes()
+        for n in my_nodes:
+            for nn in n.neighbours:
+                if nn.owner != world.my_id:
+                    world.move_army(n.index, nn.index, int(n.army_count / 2))
--- Model.py
+++ Model.py
@@ -5,60 +5,60 @@ import time
 
 class Graph:
     def __init__(self, nodes):
-        self.__nodes = nodes
-
-    def get_nodes(self):
-        return self.__nodes
-
-    def get_node(self, i):
-        return self.__nodes[i]
+        self.nodes = nodes
+    #
+    #def get_nodes(self):
+    #    return self.__nodes
+    #
+    #def get_node(self, i):
+    #    return self.__nodes[i]
 
 
 class Node:
     def __init__(self, index):
-        self.__index = index
-        self.__owner = -1
-        self.__army_count = 0
-        self.__neighbours = []
+        self.index = index
+        self.owner = -1
+        self.army_count = 0
+        self.neighbours = []
 
-    def get_neighbours(self):
-        return self.__neighbours
-
-    def get_index(self):
-        return self.__index
-
-    def get_owner(self):
-        return self.__owner
-
-    def get_army_count(self):
-        return self.__army_count
+    #def get_neighbours(self):
+    #    return self.__neighbours
+    #
+    #def get_index(self):
+    #    return self.__index
+    #
+    #def get_owner(self):
+    #    return self.__owner
+    #
+    #def get_army_count(self):
+    #    return self.__army_count
 
     def set_neighbours(self, neighbours):
-        self.__neighbours = neighbours
+        self.neighbours = neighbours
 
     def set_owner(self, owner):
-        self.__owner = owner
+        self.owner = owner
 
     def set_army_count(self, army_count):
-        self.__army_count = army_count
+        self.army_count = army_count
 
     def set_index(self, index):
-        self.__index = index
+        self.index = index
 
 
 class World:
     def __init__(self, queue):
-        self.__turn_start_time = 0
-        self.__turn_time_out = 400
-        self.__queue = queue
-        self.__turn = 0
-        self.__my_id = 0
-        self.__map = 0
-        self.__nodes = [[], [], []]
+        self.turn_start_time = 0
+        self.turn_time_out = 400
+        self.queue = queue
+        self.turn = 0
+        self.my_id = 0
+        self.map = 0
+        self.nodes = [[], [], []]
 
     def handle_init_message(self, msg):
-        self.__turn_time_out = int(msg[Constants.KEY_ARGS][0])
-        self.__my_id = int(msg[Constants.KEY_ARGS][1])
+        self.turn_time_out = int(msg[Constants.KEY_ARGS][0])
+        self.my_id = int(msg[Constants.KEY_ARGS][1])
 
         adj_list_init = msg[Constants.KEY_ARGS][2]
         nodes = []
@@ -80,60 +80,60 @@ class World:
             army_count = int(node_diff[2])
             nodes[node].set_owner(owner)
             nodes[node].set_army_count(army_count)
-        self.__map = Graph(nodes)
-        self.__update_nodes_list()
+        self.map = Graph(nodes)
+        self.update_nodes_list()
 
     def handle_turn_message(self, msg):
-        self.__turn_start_time = int(round(time.time()*1000))
-        self.__turn = int(msg[Constants.KEY_ARGS][0])
+        self.turn_start_time = int(round(time.time()*1000))
+        self.turn = int(msg[Constants.KEY_ARGS][0])
 
         graph_diff = msg[Constants.KEY_ARGS][1]
         for i in range(len(graph_diff)):
             node_diff = graph_diff[i]
             node_index = int(node_diff[0])
-            self.__map.get_node(node_index).set_owner(int(node_diff[1]))
-            self.__map.get_node(node_index).set_army_count(int(node_diff[2]))
-        self.__update_nodes_list()
+            self.map.nodes[node_index].set_owner(int(node_diff[1]))
+            self.map.nodes[node_index].set_army_count(int(node_diff[2]))
+        self.update_nodes_list()
 
-    def __update_nodes_list(self):
+    def update_nodes_list(self):
         # dummies! :))
         nodes_list = [[],[],[]]
-        for n in self.__map.get_nodes():
-            nodes_list[n.get_owner() + 1].append(n)
+        for n in self.map.nodes:
+            nodes_list[n.owner + 1].append(n)
 
-        for i in range(len(self.__nodes)):
-            self.__nodes[i] = nodes_list[i]
+        for i in range(len(self.nodes)):
+            self.nodes[i] = nodes_list[i]
             #print(nodes_list[i], file=sys.stderr)
 
     def get_turn_time_passed(self):
-        return int(round(time.time()*1000)) - self.__turn_start_time
+        return int(round(time.time()*1000)) - self.turn_start_time
 
     def get_turn_remaining_time(self):
-        return self.__turn_time_out - self.get_turn_time_passed()
+        return self.turn_time_out - self.get_turn_time_passed()
 
-    def get_my_id(self):
-        return self.__my_id
-
-    def get_map(self):
-        return self.__map
+    #def get_my_id(self):
+    #    return self.__my_id
+    #
+    #def get_map(self):
+    #    return self.__map
 
     def get_my_nodes(self):
-        return self.__nodes[self.__my_id + 1]
+        return self.nodes[self.my_id + 1]
 
     def get_opponent_nodes(self):
-        return self.__nodes[2 - self.__my_id]
+        return self.nodes[2 - self.my_id]
 
     def get_free_nodes(self):
-        return self.__nodes[0]
+        return self.nodes[0]
 
-    def get_turn_number(self):
-        return self.__turn
-
-    def get_total_turn_time(self):
-        return self.__turn_time_out
+    #def get_turn_number(self):
+    #    return self.__turn
+    #
+    #def get_total_turn_time(self):
+    #    return self.__turn_time_out
 
     def move_army(self, src, dst, count):
-        self.__queue.put(Event('m', [src, dst, count]))
+        self.queue.put(Event('m', [src, dst, count]))
 
 
 class Event:
